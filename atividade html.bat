385c68c182d43cceb9dd52171c8d90afbddbf395
clarkexemplo <marloexemplo.com>
2023-03-29 17:05:06 -0300

    formulario

---
+++ atividade html.bat
@@ -0,0 +1,27 @@
+<!DOCTYPEhtml >
+< html  lang =" pt-br " >
+< cabeça >
+    < meta  charset =" UTF-8 " >
+    < meta  http-equiv =" X-UA-Compatible " content =" IE=edge " >
+    < meta  name =" janela de visualização " conteúdo =" largura=largura do dispositivo, escala inicial=1,0 " >
+    < title > Formulário de cadastro </ title >
+</ cabeça >
+< corpo >
+    < título > formulário </ título >
+</ cabeça >
+< corpo >
+    < h1 > Formulário de cadastro </ h1 >
+    < formulário >
+        < label > Seu nome </ label >
+        < input  type =" text " placeholder =" seu nome " />
+        < br />
+        < label  for =" email " > Seu e-mail </ label >
+        < input  id =" email " type =" email " />
+        < br />
+        < label  for =" senha " > Senha </ label >
+        < input  id =" senha " type =" senha " />
+        < button  type =" submit " > Salvar </ button >
+    </forma> _ _
+
+</ corpo >
+</html> _ _
